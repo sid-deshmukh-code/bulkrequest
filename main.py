cf7da2205381b27678577a74c43bb3e6c7728c66
Sid <deshmukhsiddhant420@gmail.com>
2023-02-20 23:20:37 +0530

    listed all websites from csv file

--- main.py
+++ main.py
@@ -2,5 +2,21 @@ import csv
 
 with open('top500Domains.csv', 'r') as file:
     reader = csv.reader(file)
+    
+
+    web_list = []
+
+
     for row in reader:
-        print(row)
+        web_list.append(row[1])
+
+
+print(web_list)
+
+        
+        
+
+    
+
+#----------------------------------------------------------
+
