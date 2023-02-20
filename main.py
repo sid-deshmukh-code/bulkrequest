1df801db10115809f586239d4098cab1f8e4cd37
Sid <deshmukhsiddhant420@gmail.com>
2023-02-21 00:11:43 +0530

    success

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import csv
-
+import requests
+count = 0
 with open('top500Domains.csv', 'r') as file:
     reader = csv.reader(file)
     
 
     web_list = []
 
-
+    # getting list of top 500 websites from 
     for row in reader:
         web_list.append(row[1])
 
-
+web_list.pop(0)
 print(web_list)
+
+
+for webs in web_list:
+    try:
+        response = requests.get(f"https://{webs}")
+        print(f"{webs}-----------{response.status_code}")
+        
+    except:
+        count += 1
+        continue
+
+
+print(f"Errors: {count}")
 
         
         
 
     
 
-#----------------------------------------------------------
+
 
